1a6d0ff813b31c2b209bc879bf7a895093793bd4
LeoSCorreia <leandrocorreia.300@gmail.com>
2022-09-17 20:47:45 -0300

    adição de img and js

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,27 @@
 var listaDeFilmes = [
   "https://i.im.ge/2022/09/18/126ecJ.yKFaC1fj4hzEOrMzvIYuuZyCTx8-185x278.jpg",
   "https://i.im.ge/2022/09/18/12NMey.Mewtwo-Contra-Ataca.jpg",
-  "https://static.wikia.nocookie.net/pokepediabr/images/0/08/Giratina_and_the_Sky_Warrior_rerelease.jpg/revision/latest/scale-to-width-down/250?cb=20161216180857&path-prefix=pt-br",
+  "https://i.im.ge/2022/09/18/12N051.Giratina-and-the-Sky-Warrior-rerelease.webp",
 ];
+
+document.ready = () => {
+  for (var i = 0; i < listaDeFilmes.length; i++) {
+    document.write("<img src=" + listaDeFilmes[i] + "> ");
+  }
+};
+document.ready();
 var image = document.getElementById("Filmes");
 
-for (var i = 0; i < listaDeFilmes.length; i++) {
-  document.write("<img src=" + listaDeFilmes[i] + ">");
+function confirmar() {
+  var url = document.getElementById("URL").value;
+  var verif = listaDeFilmes.includes(url);
+
+  if (verif == true) {
+    alert("Filme já adicionado");
+  } else {
+    listaDeFilmes.push(url.value);
+    // var teste = document.getElementById("filmes");
+    // var teste2 = "<img src=" + url.value + "> ";
+    // teste.innerHTML += teste2;
+  }
 }
