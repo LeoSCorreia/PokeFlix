2d73ec8cbabbfd54548044bba32b4f8ea62f5ff4
LeoSCorreia <leandrocorreia.300@gmail.com>
2022-09-18 17:19:01 -0300

    Last att

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,15 @@
-var listaDeFilmes = [
-  "https://i.im.ge/2022/09/18/126ecJ.yKFaC1fj4hzEOrMzvIYuuZyCTx8-185x278.jpg",
-  "https://i.im.ge/2022/09/18/12NMey.Mewtwo-Contra-Ataca.jpg",
-  "https://i.im.ge/2022/09/18/12N051.Giratina-and-the-Sky-Warrior-rerelease.webp",
-];
+function adicionarFilme() {
+  var url = document.getElementById("filme").value;
 
-document.ready = () => {
-  for (var i = 0; i < listaDeFilmes.length; i++) {
-    document.write("<img src=" + listaDeFilmes[i] + "> ");
-  }
-};
-document.ready();
-var image = document.getElementById("Filmes");
-
-function confirmar() {
-  var url = document.getElementById("URL").value;
-  var verif = listaDeFilmes.includes(url);
-
-  if (verif == true) {
-    alert("Filme já adicionado");
+  if (url.endsWith(".jpg") || url.endsWith(".png")) {
+    listarFilmeNaTela(url);
   } else {
-    listaDeFilmes.push(url.value);
-    // var teste = document.getElementById("filmes");
-    // var teste2 = "<img src=" + url.value + "> ";
-    // teste.innerHTML += teste2;
+    alert("Endereço de filme Invalido!");
   }
+  document.getElementById("filme").value = "";
+}
+function listarFilmeNaTela(filme) {
+  var elementoFilmeFavorito = "<img src=" + filme + ">";
+  var listaFilmes = document.getElementById("listaFilmes");
+  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilmeFavorito;
 }
